e67b333b94189e5ececfe4cb3990aee3a6605ddd
Hongwei Ye <yehwhey@gmail.com>
2023-12-03 20:31:42 +0800

    Refactor: add docstrings

--- tests/test_igv.py
+++ tests/test_igv.py
@@ -1,3 +1,10 @@
+"""
+Dash App for Genomic Data Visualization
+
+This module sets up a Dash application to visualize genomic variant data using the Integrative Genomics Viewer (IGV).
+It serves variant call format (VCF) files and their index files (TBI) from a specified directory within the package.
+"""
+
 import importlib.resources
 from pathlib import Path
 
@@ -7,31 +14,38 @@ from dash import html
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 
+# Access the data directory within the 'tests.data' package.
 DATA_DIR = importlib.resources.files("tests.data")
 assert isinstance(DATA_DIR, Path) and DATA_DIR.is_dir()
 
+# Constants for sample name and file names.
 SAMPLE_NAME = "SRR2980466"
-
 SAMPLE_VCF = "SRR2980466_output_TD.vcf.gz"
-assert DATA_DIR.joinpath(SAMPLE_VCF).is_file()
-
 SAMPLE_VCF_INDEX = "SRR2980466_output_TD.vcf.gz.tbi"
+
+# Ensure the VCF and its index file exist in the data directory.
+assert DATA_DIR.joinpath(SAMPLE_VCF).is_file()
 assert DATA_DIR.joinpath(SAMPLE_VCF_INDEX).is_file()
 
+# Constants for genomic data.
 GENOME = "hg38"
 REGION = "chr13:28,046,009-28,050,715"
-
 ROUTE_ROOT = "data"
 
-
+# Initialize Dash app and Flask server.
 APP = dash.Dash(__name__)
 server = APP.server
-
 assert isinstance(server, Flask)
 
 
 @server.route(f"/{ROUTE_ROOT}/<path:path>")
 def send_data(path):
+    """
+    Serve a file from the data directory.
+
+    :param path: Path to the file within the data directory.
+    :return: Flask response object to send the file.
+    """
     APP.logger.info(f"Serving file: {path}")
     try:
         response = send_from_directory(str(DATA_DIR), path, as_attachment=True)
@@ -43,30 +57,23 @@ def send_data(path):
         raise e
 
 
-@server.after_request
-def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-    return response
-
-
-# Enable CORS for all routes
-CORS(server, resources={r"/track_data/*": {"origins": "*"}})
+# Enable CORS for all routes using the specified root.
+CORS(server, resources={rf"/{ROUTE_ROOT}/*": {"origins": "*"}})
 
 
 def make_igv():
-    genome = GENOME
-    region = REGION
-    vcf = f"{ROUTE_ROOT}/{SAMPLE_VCF}"
-    vcf_index = f"{ROUTE_ROOT}/{SAMPLE_VCF_INDEX}"
+    """
+    Create the IGV component for the Dash app.
+
+    :return: IGV component configured with genomic data tracks.
+    """
     track_data = [
         {
             "type": "variant",
             "format": "vcf",
-            "url": vcf,
-            "indexURL": vcf_index,
-            "name": f"{SAMPLE_NAME} FLT3-ITD",
+            "url": f"{ROUTE_ROOT}/{SAMPLE_VCF}",
+            "indexURL": f"{ROUTE_ROOT}/{SAMPLE_VCF_INDEX}",
+            "name": f"{SAMPLE_NAME} FLT3-ITD variants",
             "squishedCallHeight": 1,
             "expandedCallHeight": 4,
             "displayMode": "EXPANDED",
@@ -78,13 +85,15 @@ def make_igv():
 
     return dash_bio.Igv(
         id="FLT3-ITD",
-        genome=genome,
-        locus=region,
+        genome=GENOME,
+        locus=REGION,
         tracks=track_data,
     )
 
 
+# Define the layout of the Dash app to include the IGV component.
 APP.layout = html.Div([make_igv()])
 
 if __name__ == "__main__":
+    # Run the Dash app server on port 18050 in debug mode.
     APP.run_server(debug=True, port=18050)
